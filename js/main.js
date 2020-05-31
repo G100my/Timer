7128e7582d9a7cc213ad8785528f22c0cc7a71fd
G100my <G100my.lan229@gmail.com>
2020-06-01 01:57:52 +0800

    refactor: 改成只把 計數器timer 的部分獨立出來

---
+++ js/main.js
@@ -0,0 +1,215 @@
+let stampState = true;
+
+// 紀錄接下來的事件、已經完成準備上傳到 sheet 的事件
+var list = getLocal('localList') || {
+  toDo: [],
+  complete: [],
+  lastTitle: ''
+};
+// 紀錄當前工作事件資料，每次完成、中止都會記錄到 list.complete
+var mission = {
+  startTime: undefined,
+  name: '',
+  minSet: undefined,
+  completed: false,
+};
+let minSet = 1; //fix
+let input = document.getElementById("mission_input_btn");
+let current_mission_display = document.getElementById("current_mission");
+
+// =================== normal clock
+
+var clockTime;
+var clock;
+setInterval(() => {
+  clock = new Date();
+  let h = clock.getHours();
+  let m = clock.getMinutes();
+  let s = clock.getSeconds();
+
+  h = (h < 10) ? "0" + h : h;
+  m = (m < 10) ? "0" + m : m;
+  clockTime = h + ' : ' + m;
+
+  $("#digital_clock").text(clockTime);
+}, 1000);
+
+// ===================
+
+function addToList(textValue) {
+  console.log('addToList: ');
+  let id = Date.now();
+  list.toDo.push({ id: id, name: textValue });
+  setLocal('localList', list);
+  $("#mission_list").append(
+    '<p id="' + id + '" class="missions">' + textValue + "<button onclick='deleteMission(" + id + ")'>delete</button></p>");
+};
+
+function deleteMission(id) {
+  console.log('deleteMission');
+  list.toDo.pop();
+  $("#" + id).remove();
+  setLocal('localList', list);
+};
+
+// ===================
+
+function checkNext() {
+  console.log('checkout');
+  if (timer.combo >= 4) {
+    console.log('check combo-4');
+    setSmokeCall(1, 0);
+    displayMissionTitle('take a walk!');
+    return;
+  };
+  if (document.getElementById('repeat_switch').checked) {
+    console.log('check repeat');
+    timer.set(mission.minSet);
+    mission.completed = false;
+    timer.start(() => {
+      finishMission();
+      setSmokeCall();
+    });
+    return;
+  };
+
+  $("#start_btn").show();
+  $("#stop_btn").hide();
+};
+
+function finishMission(completed = true) {
+  console.log('finishMission() ' + completed)
+  mission.completed = completed;
+  list.complete.push(mission);
+  if (completed) { timer.combo += 1; };
+  setLocal('localList', list);
+};
+
+function setNextNameMin(minSet = 25) {
+  console.log('setNextNameMin ' + minSet);
+  mission.minSet = minSet;
+  timer.set(minSet);
+  if (list.toDo.length > 0) {
+    let toDo = list.toDo.shift();
+    $(".missions:first").remove();
+    mission.name = toDo.name;
+  } else {
+    mission.name = 'Do your best';
+  };
+  displayMissionTitle(mission.name);
+};
+
+function setSmokeCall(min = 5) {
+  console.log('setSmokeCall: ' + min + ' ' + sec);
+  timer.set(min);
+  timer.start(checkNext);
+};
+
+// ===================
+
+function loadLocal() {
+  if (list.toDo.length > 0) {
+    console.log('load local list');
+    console.log(list);
+    list.toDo.forEach(e => {
+      $("#mission_list").append(
+        '<p id="' + e.id + '" class="missions">' + e.name + "<button onclick='deleteMission(" + e.id + ")'>delete</button></p>");
+    });
+  };
+  if (mission.startTime !== undefined && !mission.completed) {
+    console.log('load local mission');
+    console.log(mission);
+    t = ((mission.startTime + mission.minSet * 60000) - Date.now()) / 1000;
+    if (t > 1) {
+      let m = parseInt((t) / 60);
+      let s = parseInt(t % 60)
+      timer.set(m, s);
+      timer.start(setSmokeCall);
+      displayMissionTitle(mission.name);
+
+      $("#start_btn").hide();
+      $("#stop_btn").show();
+    }
+  };
+};
+
+// ===================
+
+function setLocal(key, item) {
+  console.log('setLocal ' + key);
+  localStorage.setItem(key, JSON.stringify(item));
+  console.log(item);
+  console.log('local storage: ');
+  console.log(localStorage);
+};
+
+function getLocal(item) {
+  let v = JSON.parse(localStorage.getItem(item));
+  return (v === null) ? false : v;
+};
+
+function displayMissionTitle(msg) {
+  document.getElementById("current_mission").innerText = msg;
+};
+
+$('#sheet_init').click(function () {
+  console.log('click init');
+  initSheet(timerSheetID);
+});
+
+$('#stamp_start').click(function () {
+  stampState = !stampState;
+  startStamp();
+  $('#stamp_start').hide();
+  $('#stamp_end').show();
+});
+
+$('#stamp_end').click(function () {
+  stampState = !stampState;
+  endStamp();
+  $('#stamp_end').hide();
+  $('#stamp_start').show();
+});
+
+$("#mission_input_btn").keyup((event) => {
+  //enter鍵：
+  // 如果不曾輸入過文字：null；
+  // 如果曾經送出過文字、但目前留空：上次輸入的文字；
+  let currenValue = (input.value === '') ? ((list.lastTitle === undefined) ? '' : list.lastTitle) : input.value;
+  if (event.which === 13 && (currenValue !== '' || list.lastTitle !== '')) {
+    list.lastTitle = currenValue;
+    addToList(currenValue);
+    input.value = '';
+  };
+  //esc鍵：清除目前所鍵入的文字
+  (event.which === 27 && input.value !== '' && (input.value = ''));
+});
+
+// 按下後與 stop 互換顯示狀態，目標: 不會有還沒中止就能再按一次開始，
+$("#start_btn").click(() => {
+  setNextNameMin();
+  mission.startTime = Date.now();
+  mission.minSet = minSet;
+  timer.start(() => {
+    finishMission();
+    setSmokeCall();
+    displayMissionTitle('SmokeCall');
+  });
+  setLocal('localMission', mission);
+  setLocal('localList', list);
+  $("#start_btn").hide();
+  $("#stop_btn").show();
+});
+
+$("#stop_btn").click(() => {
+  timer.stop();
+  finishMission(false);
+  displayMissionTitle('stop');
+
+  checkNext();
+  // reset SVG
+  displayMissionTitle('manual stop');
+  document.getElementById("tomato_clock").innerText = '--:--';
+  // re
+
+});
--- js/timer.js
+++ js/timer.js
@@ -1,237 +1,94 @@
-let stampState = true;
-
+// 當前計數器狀態，如果是當前事件循環的狀況、則重新設定歸零再次計數、mission資料則部份改變不重置
 var timer = {
-  min: undefined,
-  sec: undefined,
-  num: undefined,
-  start: undefined
-  stop: () => clearInterval(timer.num),
-  countSec: 0,
-  combo: 0,
-};
-var list = getLocal('localList') || {
-  toDo: [],
-  complete: [],
-  lastTitle: undefined
-};
-var emptyMission = {
-  startTime: undefined,
-  name: undefined,
-  minSet: undefined,
-  completed: false,
-  repeat: false,
-  combo: 0
-};
-var mission = getLocal('localMission') || Object.assign({}, emptyMission);
-let input = document.getElementById("mission_input_btn");
-let current_mission_display = document.getElementById("current_mission");
-
-// =================== normal clock
-
-var clockTime;
-var clock;
-setInterval(() => {
-  clock = new Date();
-  let h = clock.getHours();
-  let m = clock.getMinutes();
-  let s = clock.getSeconds();
-
-  h = (h < 10) ? "0" + h : h;
-  m = (m < 10) ? "0" + m : m;
-  clockTime = h + ' : ' + m;
-
-  $("#digital_clock").text(clockTime);
-}, 1000);
-
-// ===================
-
-function addToList(textValue) {
-  let id = Date.now();
-  list.toDo.push({ id: id, name: textValue });
-  setLocal('localList', list);
-  $("#mission_list").append(
-    '<p id="' + id + '" class="missions">' + textValue + "<button onclick='deleteMission(" + id + ")'>delete</button></p>");
+	min: 0,
+	sec: 0,
+	num: 0,
+	// countSec: 10,
+	combo: 0,
+	startTime: 0,
+	set: (min, sec = 0) => {
+		timer.min = min;
+		timer.sec = sec;
+		timer.minSet = min * 60000 + sec * 1000;
+		displayTime();
+	},
+	stop: () => { clearInterval(timer.num); },
+	start: (callback = () => { console.log('No next mission? Really?') }) => {
+		let countSec = 10;
+		timer.startTime = Date.now();
+		timer.num = setInterval(() => {
+			console.log('startime ' + timer.startTime);
+			console.log('timer.minset ' + timer.minSet);
+			console.log(countSec);
+			if (countSec === 10) {
+				displayTime();
+				timer.sec -= 1;
+				countSec = 0;
+			};
+			
+			if (timer.min == 0 && timer.sec < 0) {
+				clearInterval(timer.num);
+				callback();
+			};
+			if (timer.sec < 0) {
+				timer.sec = 59;
+				timer.min -= 1;
+			};
+			let n = (Date.now() - timer.startTime) / (timer.minSet);
+			drawProgress((n > 1) ? 1 : n);
+			countSec += 1;
+		}, 100);
+	},
 };
 
-function deleteMission(id) {
-  list.toDo.pop();
-  $("#" + id).remove();
-  setLocal('localList', list);
+// FIXME
+var node = document.getElementById('progress').parentElement;
+var h = node.clientHeight;
+var w = node.clientWidth;
+
+window.onresize = () => {
+	node = document.getElementById('progress').parentElement;
+	h = node.clientHeight;
+	w = node.clientWidth;
 };
 
-function checkNext() {
-  if (combo >= 4) {
-    setSmokeCall(20, 0);
-    displayMission('take a walk!');
-    return;
-  };
-  if (mission.repeat) {
-    setCountDown(mission.minSet);
-    mission.completed = false;
-    timer.start(() => {
-      finishMission();
-      setSmokeCall();
-    });
-    return;
-  };
-  setNextMission();
-  setCountDown(mission.minSet);
-  displayMission(mission.name);
-  $("#start_btn").show();
-  $("#stop_btn").hide();
-};
-
-function countXYR(width, height, num) {
-  let length = (width <= height) ? width / 2 : height / 2;
-  let x = y = length;
-  let outside_r = (width <= height) ? width / 2 : height / 2;
-  let inside_r = outside_r * 0.7; 
-  return {
-    x: x,
-    y: y,
-    r: outside_r,
-    ir: inside_r,
-    ox: x + outside_r * Math.sin(num),
-    oy: y - outside_r * Math.cos(num),
-    ix: x + inside_r * Math.sin(num),
-    iy: y - inside_r * Math.cos(num),
-  };
-};
-
-function drawProgress(num) {
-  let degree = num * 2 * Math.PI;
-  let node = document.getElementById('progress').parentElement;
-  let h = node.clientHeight;
-  let w = node.clientWidth;
-  var c = countXYR(h, w, degree);
-  let mode = (num > 0.5) ? '1' : '0';
-  $('#progress').attr('d',
-    'M ' + c.x + ' ' + '0' + ' ' +
-    'A ' + c.r + ' ' + c.r + ' ' + '0 ' + mode + ' 1 ' + c.ox + ' ' + c.oy + ' ' +
-    'L ' + c.x + ' ' + c.y + ' Z' + ' '
-    + 'M ' + c.x + ' ' + (c.r - c.ir) + ' ' +
-    'A ' + c.ir + ' ' + c.ir + ' ' + '0 ' + mode + ' 1 ' + c.ix + ' ' + c.iy + ' ' +
-    'L ' + c.x + ' ' + c.y + ' Z'
-  );
-};
-
-function finishMission(completed = true) {
-  mission.completed = completed;
-  completed && (mission.combo += 1);
-  list.complete.push(mission);
-
-  mission = Object.assign({}, emptyMission);
-  setLocal('localMission', mission);
-};
-
-function setNextMission(min = 25) {
-  mission.Date = clock.toLocaleDateString();
-  mission.minSet = min;
-  setCountDown(min);
-  // 允許沒有輸入事件、行程
-  if (list.toDo.length > 0) {
-    console.log('ggg');
-    let toDo = list.toDo.shift();
-    $(".missions:first").remove();
-    mission.name = toDo.name;
-  } else {
-    mission.name = 'Do your best';
-  };
-  displayMission(mission.name);
-};
-
-function setSmokeCall(min = 5, sec = 0) {
-  setCountDown(min, sec);
-  timer.start(checkNext);
-};
-
-function setCountDown(min = 25, sec = 0) {
-  timer.min = min;
-  timer.sec = sec;
-  timer.start = (callback = () => { console.log('No next mission? Really?') }) => {
-    timer.num = setInterval(() => {
-      timer.countSec += 1;
-      drawProgress((Date.now() - mission.startTime) / (mission.minSet * 60000));
-
-      if (timer.countSec === 10) {
-        let min_sec = addZero(timer.min) + ':' + addZero(timer.sec);
-        displayTimer(min_sec);
-        timer.sec -= 1;
-        timer.countSec = 0;
-      };
-
-      if (timer.min == 0 && timer.sec < 0) {
-        clearInterval(timer.num);
-        callback();
-      };
-      if (timer.sec < 0) {
-        timer.sec = 59;
-        timer.min -= 1;
-      };
-    }, 100);
-  };
-  timer.stop = () => clearInterval(timer.num);
-};
-
-function setCountUp(min = 0, sec = 0) {
-  timer.min = min;
-  timer.sec = sec;
-  timer.start = () => {
-    timer.num = setInterval(() => {
-      timer.sec += 1;
-      if (timer.sec >= 60) {
-        timer.sec = 0;
-        timer.min += 1;
-      };
-      let min_sec = addZero(timer.min) + ':' + addZero(timer.sec);
-      displayTimer(min_sec);
-    }, 1000);
-  };
-  timer.stop = () => clearInterval(timer.num);
-};
-
-function loadLocal() {
-  if (list.toDo.length > 0) {
-    list.toDo.forEach(e => {
-      $("#mission_list").append(
-        '<p id="' + e.id + '" class="missions">' + e.name + "<button onclick='deleteMission(" + e.id + ")'>delete</button></p>");
-    });
-  };
-  if (mission.startTime !== undefined) {
-    t = ((mission.startTime + mission.minSet * 60000) - Date.now()) / 1000;
-
-    if (t > 1) {
-      let m = parseInt((t) / 60);
-      let s = parseInt(t % 60)
-      setCountDown(m, s);
-      timer.start(setSmokeCall);
-      displayMission(mission.name);
-
-      $("#start_btn").hide();
-      $("#stop_btn").show();
-    }
-  };
-};
-
-// =================== 
-
-function displayTimer(msg) {
-  document.getElementById("tomato_clock").innerText = msg;
-};
-
-function displayMission(msg) {
-  document.getElementById("current_mission").innerText = msg;
+function displayTime(m = timer.min, s = timer.sec) {
+	document.getElementById("tomato_clock").innerText = addZero(m) + ':' + addZero(s);
+	console.log('displayTime:  m ' + m + 's ' + s);
 };
 
 function addZero(num) {
-  return (num < 10) ? '0' + num : num;
+	return (num < 10) ? '0' + num : num;
 };
 
-function setLocal(key, item) {
-  localStorage.setItem(key, JSON.stringify(item));
+function countXYR(width, height, num) {
+	let x = y = (width <= height) ? width / 2 : height / 2;
+	let outside_r = x * 0.95;
+	let inside_r = outside_r * 0.7;
+	return {
+		x: x,
+		y: y,
+		or: outside_r,
+		ir: inside_r,
+		ox: x + outside_r * Math.sin(num),
+		oy: y - outside_r * Math.cos(num),
+		ix: x + inside_r * Math.sin(num),
+		iy: y - inside_r * Math.cos(num),
+	};
 };
 
-function getLocal(item) {
-  let v = JSON.parse(localStorage.getItem(item));
-  return (v === null) ? false : v;
+function drawProgress(num) {
+	let degree = num * 2 * Math.PI;
+	var c = countXYR(h, w, degree);
+	let mode = (num > 0.5) ? '1' : '0';
+	$('#progress').attr('d',
+		'M ' + c.x + ' ' + (c.y - c.or) + ' ' +
+		'A ' + c.or + ' ' + c.or + ' ' + '0 ' + mode + ' 1 ' + c.ox + ' ' + c.oy + ' ' +
+		'L ' + c.x + ' ' + c.y + ' Z' + ' '
+		+ 'M ' + c.x + ' ' + (c.y - c.ir) + ' ' +
+		'A ' + c.ir + ' ' + c.ir + ' ' + '0 ' + mode + ' 1 ' + c.ix + ' ' + c.iy + ' ' +
+		'L ' + c.x + ' ' + c.y + ' Z'
+	);
+	console.log(c);
+	console.log('num ' + num);
 };
