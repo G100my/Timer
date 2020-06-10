fb8935a7dfcadad1af35085bc37a44d1a285b4e3
G100my <G100my.lan229@gmail.com>
2020-06-11 03:37:43 +0800

    feat: basic

---
+++ js/main.js
@@ -0,0 +1,270 @@
+let stampState = true;
+let defaultMissionTitle = 'Do your best';
+let SmokeCallMsg = 'Take a breath.';
+let RestMsg = 'Take a walk.';
+let input = document.getElementById("mission_input");
+let restTime = 20;
+
+
+var list = getLocal('list') || {
+  toDo: [],
+  completed: [],
+  lastTitle: defaultMissionTitle
+};
+
+var mission = getLocal('mission') || {
+  combo: 0,
+  repeat: false,
+  name: defaultMissionTitle,
+  minSet: 25,
+  startTime: 0
+};
+
+
+// ==== input mission
+
+// true => reset last mission's forecast time, false => reset all
+function setForecastTime(mode = false) {
+  let forecast = document.getElementsByClassName('forecast');
+  if (forecast.length == 0) { return };
+  for (let i = (mode) ? forecast.length - 1 : 0; i < forecast.length; i++) {
+    let h = clock.getHours();
+    let m = clock.getMinutes() + mission.minSet * (i + 1) + 5;
+    if (m >= 60) {
+      h += parseInt(m / 60);
+      m = m % 60;
+    };
+    if (h >= 24) {
+      h -= 24;
+    };
+    forecast[i].innerText = addZero(h) + ':' + addZero(m);
+  };
+};
+
+function addTo_mission_list(id, string) {
+  $("#mission_list").append(
+    '<div id="' + id + '" class="missions"><button class="icon_button" type="button" onclick="deleteMission(' + id + ')"><svg class="bi bi-trash-fill" width="1.2rem" height="1.2rem" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5a.5.5 0 0 0-1 0v7a.5.5 0 0 0 1 0v-7z" /></svg ></button>' + string + '<span class="forecast">00:00</span></div>');
+};
+
+function addToList(msg) {
+  let currenValue = (msg === '') ? list.lastTitle : msg;
+  list.lastTitle = currenValue;
+
+  let id = Date.now();
+  list.toDo.push(currenValue);
+  addTo_mission_list(id, currenValue);
+
+  saveList();
+  setForecastTime(true);
+};
+
+function deleteMission(id) {
+  let target = $("#" + id);
+  let index = list.toDo.findIndex(t => t === target.text());
+  list.toDo.splice(index, 1);
+  target.remove();
+  
+  saveList();
+  setForecastTime(0);
+};
+
+$('#mission_input_btn').click(() => {
+  addToList(input.value);
+  input.value = '';
+});
+
+$("#mission_input").keyup((event) => {
+  if (event.which === 13 && (input.value !== '' || list.lastTitle !== '')) {
+    addToList(input.value);
+    input.value = '';
+  };
+  (event.which === 27 && input.value !== '' && (input.value = ''));
+});
+
+
+// ==== start button & stop button
+
+function displayMissionTitle(msg) {
+  document.getElementById("current_mission").innerText = msg;
+};
+
+function finishSmokeCall() {
+  if (!mission.repeat) {
+    let next = $("#mission_list div:first");
+    mission.name = next.text();
+  };
+  numberAppear();
+  $("#start_btn").show();
+  $("#stop_btn").hide();
+};
+
+function setNextSmokeCall() {
+  if (mission.combo >= 4) {
+    mission.combo = 0;
+    mission.name = restTime;
+    timer.set(restTime, 0, 0);
+  } else {
+    mission.combo += 1;
+    mission.name = SmokeCallMsg
+    timer.set(5, 0, 0);
+  }
+  saveMission();
+  timer.start(finishSmokeCall);
+};
+
+
+$("#start_btn").click(() => {
+  mission.name = (list.toDo.length > 0) ? list.toDo[0] : defaultMissionTitle;
+  mission.startTime = Date.now();
+
+  timer.set(mission.minSet);
+  timer.start(() => {
+    finishMission();
+    setNextSmokeCall();
+  });
+  
+  $("#start_btn").hide();
+  $("#stop_btn").show();
+  setForecastTime();
+  numberDisappear();
+  saveMission();
+});
+
+$("#stop_btn").click(() => {
+  timer.stop();
+  finishMission(false);
+
+  drawProgress(0);
+  displayMissionTitle('Manual Stop');
+  document.getElementById("tomato_clock").innerText = '--:--';
+
+  numberAppear();
+  $("#start_btn").show();
+  $("#stop_btn").hide();
+  
+  setForecastTime();
+});
+
+
+// ==== data
+
+function finishMission(isCompleted = true) {
+  list.completed.push({
+    title: mission.name,
+    startTime: mission.startTime,
+    timeSet: mission.minSet,
+    completed: (isCompleted) ? mission.minSet : mission.minSet - timer.min - 1
+  });
+  if (list.toDo.length >= 1 && isCompleted && !mission.repeat) {
+    mission.name = list.toDo.shift();
+    $(".missions:first").remove();
+  };
+  mission.startTime = undefined;
+  saveList();
+  saveMission();
+};
+
+function saveMission() {
+  localStorage.setItem('mission', JSON.stringify(mission));
+};
+
+function saveList() {
+  localStorage.setItem('localList', JSON.stringify(list));
+};
+
+function getLocal(item) {
+  let v = JSON.parse(localStorage.getItem(item));
+  return (v === null) ? false : v;
+};
+
+function loadLocal() {
+  if (list.toDo.length > 0) {
+    let index = 0;
+    list.toDo.forEach(e => {
+      addTo_mission_list(index, e);
+      index += 1;
+    });
+  };
+  if (mission.startTime !== undefined) {
+    t = ((mission.startTime + mission.minSet * 60000) - Date.now()) / 1000;
+    if (t > 1) {
+      let m = parseInt((t) / 60);
+      let s = parseInt(t % 60);
+      let angle = (mission.minSet * 60 - t) / (mission.minSet * 60);
+      timer.set(m, s, angle);
+      timer.start(setNextSmokeCall);
+      numberDisappear();
+      $("#start_btn").hide();
+      $("#stop_btn").show();
+    } else {
+      finishMission();
+    }
+  };
+  setForecastTime();
+};
+
+
+// ==== number Disappear & Appear
+
+function numberDisappear() {
+  let x = document.getElementById('number_group').clientWidth / 2;
+  let y = document.getElementById('number_group').clientHeight / 2;
+  // 還不明白為什麼 $('p').each(()=>{$(this)}) 裡面的 $(this) 會指向 window .......
+  $('#number_group div').each(function () {
+    let targetX = x - $(this).position().left;
+    let targetY = y - $(this).position().top;
+    let stepX = (targetX) / 150;
+    let stepY = (targetY) / 200;
+    let step = 0;
+    let t = setInterval(() => {
+      if (step >= 100) clearInterval(t);
+
+      if (step <= 5) {
+        let scale = (1 + step * 0.025);
+        $(this).css('transform', 'matrix(' + scale + ', 0, 0, ' + scale + ', ' + (-stepX * step) + ', ' + (-stepY * step) + ')');
+        step += 0.1;
+      };
+
+      if (step > 5 && step < 20) {
+        step += 1;
+      };
+
+      if (step > 20) {
+        let scale = (1 - step * 0.01);
+        scale = (scale < 0.1) ? 0 : scale;
+        $(this).css('transform', 'matrix(' + scale + ', 0, 0, ' + scale + ', ' + stepX * step + ', ' + stepY * step + ')');
+        step += 5;
+
+        if (scale === 0) {
+          $('#number_group').hide();
+          clearInterval(t);
+          $(this).css('transform', "none");
+        };
+      }
+    }, 10);
+  })
+};
+
+function numberAppear() {
+  let step = 0;
+  let group = $('#number_group');
+  group.css({ 'opacity': '0.0' }).show();
+
+  let t = setInterval(() => {
+    if (step >= 1) clearInterval(t);
+    group.css('opacity', step)
+    step += 0.05;
+  }, 50);
+};
+
+
+// ==== oters
+
+$('#sidebarCollapse').on('click', () => {
+  $('#sidebar').toggleClass('active');
+});
+
+$('#repeat_switch').click(() => {
+  mission.repeat = !mission.repeat;
+  $('#repeat_switch').toggleClass('repeat_switch');
+});
---
+++ js/timer.js
@@ -0,0 +1,138 @@
+// ==== normal clock
+
+var clockTime;
+var clock = new Date();
+
+setInterval(() => {
+	clock = new Date();
+	let h = clock.getHours();
+	let m = clock.getMinutes();
+	let s = clock.getSeconds();
+
+	h = (h < 10) ? "0" + h : h;
+	m = (m < 10) ? "0" + m : m;
+	colon = (s % 2 == 0) ? " " : ":";
+	clockTime = h + colon + m;
+	
+	if (!timer.intervalID && s == 0) { setForecastTime() };
+
+	$("#digital_clock").text(clockTime);
+}, 1000);
+
+function displayTime(m = timer.min, s = timer.sec) {
+	document.getElementById("tomato_clock").innerText = addZero(m) + ':' + addZero(s);
+};
+
+function addZero(num) {
+	return (num < 10) ? '0' + num : num;
+};
+
+// ==== timer object
+
+var timer = {
+	min: 0,
+	sec: 0,
+	intervalID: 0,
+	timeSet: 0,
+	svgAngle: 0,
+	set: function (min, sec = 0, angle = 0, title = mission.name) {
+		this.min = min;
+		this.sec = sec;
+		this.timeSet = mission.minSet * 60000;
+		this.svgAngle = angle
+		displayTime();
+		displayMissionTitle(title);
+	},
+	stop: () => { clearInterval(timer.intervalID); timer.intervalID = false; },
+	start: function(callback = () => { console.log('No next mission? Really?') }) {
+		let countSec = 10;
+		this.intervalID = setInterval(() => {
+			if (countSec === 10) {
+				displayTime();
+				this.sec -= 1;
+				countSec = 0;
+			};
+			if (this.min <= 0 && this.sec < 0) {
+				clearInterval(this.intervalID);
+				callback();
+			};
+			if (this.sec < 0) {
+				this.sec = 59;
+				this.min -= 1;
+			};
+
+			countSec += 1;
+			this.svgAngle += (100 / this.timeSet);
+			drawProgress(this.svgAngle);
+		}, 100);
+	},
+};
+
+// ==== draw svg
+
+var progress = document.getElementById('progress').parentElement;
+var svgH = progress.clientHeight;
+var svgW = progress.clientWidth;
+
+// fixme
+window.onresize = () => {
+	progress = document.getElementById('progress').parentElement;
+	svgH = progress.clientHeight;
+	svgW = progress.clientWidth;
+};
+
+function countXYR(width, height, num) {
+	let x = y = (width <= height) ? width / 2 : height / 2;
+	let outside_r = x * 0.95;
+	let inside_r = outside_r * 0.7;
+	return {
+		x: x,
+		y: y,
+		or: outside_r,
+		ir: inside_r,
+		ox: x + outside_r * Math.sin(num),
+		oy: y - outside_r * Math.cos(num),
+		ix: x + inside_r * Math.sin(num),
+		iy: y - inside_r * Math.cos(num),
+	};
+};
+
+function drawProgress(percent) {
+	if (percent < 0.0005) { percent = 0 }
+	else if (percent >= 1) { percent = 0.9999 };
+	let degree = percent * 2 * Math.PI;
+	var c = countXYR(svgH, svgW, degree);
+	let mode = (percent > 0.5) ? '1' : '0';
+	$('#progress').attr('d',
+		'M ' + c.x + ' ' + (c.y - c.or) + ' ' +
+		'A ' + c.or + ' ' + c.or + ' ' + '0 ' + mode + ' 1 ' + c.ox + ' ' + c.oy + ' ' +
+		'L ' + c.x + ' ' + c.y + ' Z' + ' '
+		+ 'M ' + c.x + ' ' + (c.y - c.ir) + ' ' +
+		'A ' + c.ir + ' ' + c.ir + ' ' + '0 ' + mode + ' 1 ' + c.ix + ' ' + c.iy + ' ' +
+		'L ' + c.x + ' ' + c.y + ' Z'
+	);
+};
+
+
+// ==== min select
+
+var lastAngle = 0;
+
+function drawChoose(target, start = lastAngle) {
+	const times = 20;
+	let step = (target - start) / times;
+	if (step == 0) { return };
+	for (let i = 0; i <= times; i++) {
+		setTimeout(() => {
+			drawProgress(start + step * i);
+		}, 10 * i);
+	};
+	lastAngle = target;
+};
+
+$('p').each(function (e) {
+	$(this).click(() => {
+		mission.minSet = (e + 1) * 5;
+		drawChoose(mission.minSet / 60);
+	});
+});
